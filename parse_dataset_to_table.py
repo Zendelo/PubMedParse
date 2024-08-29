a8e94980bb598b04cd5306877084f82728ef1696
Zendelo <s3806763@student.rmit.edu.au>
2024-08-29 19:33:54 +1000

    add batch saving and filter based on abstract

--- parse_dataset_to_table.py
+++ parse_dataset_to_table.py
@@ -14,10 +14,14 @@ dataset = datasets.load_dataset("data/pubmed")
 # Article keys:
 # ['Abstract', 'ArticleTitle', 'AuthorList', 'Language', 'GrantList', 'PublicationTypeList']
 
+journals_df = pd.read_csv('psy_journals_list.csv', header=0, sep=',')
+
+
 # Iterate over the dataset and create a table
 table = []
 count = 0
 missing_abstracts = 0
+_batch = 0
 for row in dataset["train"]:
     pmid = row['MedlineCitation']['PMID']
     date_completed = row['MedlineCitation']['DateCompleted']['Year']
@@ -52,9 +56,13 @@ for row in dataset["train"]:
                  journal_abbr_med=journal_abbr_med))
     else:
         missing_abstracts += 1
+    if count % 1000000 == 0:
+        print('Processed:', count)
+        _df = pd.DataFrame.from_records(table)
+        _df.to_csv(f'psy_articles_{_batch}.csv', index=False)
+        _batch += 1
+        table = []
+        print(f'Saved batch {_batch}')
 
 print('Total number of abstracts:', count)
 print('Missing abstracts:', missing_abstracts)
-print('Total number of rows:', len(table))
-for row in pd.DataFrame(table).tail().iterrows():
-    print(row)
