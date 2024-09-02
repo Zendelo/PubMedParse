28d5c84cc2eba711dc0ab1655a904e58cabde370
Zendelo <s3806763@student.rmit.edu.au>
2024-09-02 12:10:10 +1000

    modify parsing to new structure

--- parse_dataset_to_table.py
+++ parse_dataset_to_table.py
@@ -1,11 +1,19 @@
+"""
+This script will parse the pubmed dataset and filter the articles that belong to the psychology journals.
+The script will save the filtered articles in batches of 500k rows to avoid memory issues.
+To modify the journals list, you can change the 'psy_journals_list.csv' file.
+
+The information about the pubmed tags, which serve as columns in the final data  can be found here:
+https://dtd.nlm.nih.gov/ncbi/pubmed/doc/out/180101/index.html
+
+"""
+
 from collections import defaultdict
 from time import time
 
 import datasets
 import pandas as pd
 from tqdm import tqdm
-
-MISSING_KEY = '*** MISSING ***'
 
 
 # extract and merge the unique values from the journals_df into a dictionary with combined keys
@@ -38,88 +46,89 @@ if __name__ == '__main__':
     # Extract unique values from the journals_df
     match_dict = extract_unique_journals(journals_df)
 
-    # Iterate over the dataset and create a table with only the matching journals
+    # Create a list (table) to store the filtered articles, and initialize the counters
     table = []
     count = 0
     missing_abstracts = 0
     filtered_articles = 0
     _batch = 0
+
+    # Iterate over the dataset rows and filter the articles that belong to the psychology journals
     for row in tqdm(dataset["train"]):
         medline = row['MedlineCitation']
         pmid = medline['PMID']
-        date_completed = medline['DateCompleted']['Year']
-        date_revised = medline['DateRevised']['Year']
+        # date_completed = medline['DateCompleted']['Year']
+        # date_revised = medline['DateRevised']['Year']
 
         article = medline['Article']
-        journal = medline['Journal']
+        journal = article['Journal']
+        journal_issue = journal['JournalIssue']
 
         article_abstract = article['Abstract']['AbstractText']
         article_title = article['ArticleTitle']
         article_language = article['Language']
 
-        article_journal = article['Journal']
-        article_journal_title = article_journal['Title']
-        article_journal_issn = article_journal['ISSN']
-        article_journal_abbr_iso = article_journal['ISOAbbreviation']
+        journal_title = journal['Title']
+        journal_issn = journal['ISSN']
+        journal_abbr_iso = journal['ISOAbbreviation']
 
-        if journal:
-            journal_issn = journal['ISSN']
-            journal_title = journal['Title']
-            journal_isoabbr = journal['ISOAbbreviation']
-            journal_year = journal['PubDate']['Year']
-        else:
-            journal_issn = MISSING_KEY
-            journal_title = MISSING_KEY
-            journal_isoabbr = MISSING_KEY
-            journal_year = MISSING_KEY
+        journal_year = journal_issue['PubDate']['Year']
 
         journal_abbr_med = medline['MedlineJournalInfo']['MedlineTA']
 
+        # Check if the article has an abstract
         if article_abstract:
-            if (pmid in match_dict['id']) or \
-                    (article_journal_issn.lower() in match_dict['issn'] or
-                     journal_issn.lower() in match_dict['issn']) or \
-                    (article_journal_title.lower() in match_dict['title']
-                     or journal_title.lower() in match_dict['title']) or \
-                    (article_journal_abbr_iso.lower() in match_dict['abbr'] or
-                     journal_isoabbr.lower() in match_dict['abbr'] or journal_abbr_med.lower() in match_dict['abbr']):
+            # Check if the article belongs to the psychology journals
+            if pmid in match_dict['id'] or \
+                    journal_issn.lower() in match_dict['issn'] or \
+                    journal_title.lower() in match_dict['title'] or \
+                    journal_abbr_iso.lower() in match_dict['abbr'] or journal_abbr_med.lower() in match_dict['abbr']:
+                # Increment the count of the articles
                 count += 1
+                # If the article belongs to the psychology journals, then append it to the table, as a dictionary.
+                # Each dict key will be a column in the DataFrame that will be created later.
                 table.append(
-                    dict(pmid=pmid,
-                         date_completed=date_completed,
-                         date_revised=date_revised,
-                         article_title=article_title,
-                         article_language=article_language,
-                         article_abstract=article_abstract,
-                         article_journal_title=article_journal_title,
-                         article_journal_issn=article_journal_issn,
-                         article_journal_abbr_iso=article_journal_abbr_iso,
-                         journal_title=journal_title,
-                         journal_issn=journal_issn,
-                         journal_isoabbr=journal_isoabbr,
-                         journal_year=journal_year,
-                         journal_abbr_med=journal_abbr_med))
+                    dict(
+                        PMID=pmid,
+                        # DateCompleted=date_completed,
+                        # DateRevised=date_revised,
+                        ArticleTitle=article_title,
+                        ArticleLanguage=article_language,
+                        AbstractText=article_abstract,
+                        JournalTitle=journal_title,
+                        JournalISSN=journal_issn,
+                        ISOAbbreviation=journal_abbr_iso,
+                        PubDate=journal_year,
+                        MedlineTA=journal_abbr_med
+                    ))
             else:
+                # If the article doesn't belong to the psychology journals, then increment the filtered articles count
                 filtered_articles += 1
         else:
+            # If the article doesn't have an abstract, then increment the missing abstracts count
             missing_abstracts += 1
-        # 1M rows per batch, equals to 1.2GB
-        if count > 0 and count % 1000000 == 0:
+        # Will save the data in batches of up to 500k rows, to reduce memory consumption. Smaller batch -> less memory.
+        if count > 0 and count % 500000 == 0:
             print('Processed:', count)
+            # Create a DataFrame from the table
             _df = pd.DataFrame.from_records(table)
             print(f'df info: {_df.info()}')
             print('Saving batch...')
-            _df.to_csv(f'psy_articles_{_batch}.csv', index=False)
+            # Save the DataFrame to a csv file, with the defined separator
+            _df.to_csv(f'psy_articles_{_batch}.csv', sep=',' ,index=False)
             print(f'Saved batch {_batch}')
             print('will dump the table to free memory')
             _batch += 1
+            # Clear the table to free memory
             table = []
     else:
         # Save the remaining rows
         print('Processed:', count)
+        # Create a DataFrame from the table
         _df = pd.DataFrame.from_records(table)
         print(f'df info: {_df.info()}')
         print('Saving the remaining batch...')
+        # Save the DataFrame to a csv file, with the defined separator
         _df.to_csv(f'psy_articles_{_batch}.csv', index=False)
         print(f'Saved batch {_batch}')
 
